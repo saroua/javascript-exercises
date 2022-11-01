a0e06596347d6927d7bed51b1269e7dadb9cc050
Ludovic Orban <luluorban2@hotmail.com>
2022-11-01 22:39:07 +0100

    First exercice solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string , count) {
+    resultat = ''
+    if (count > 0){
+        for (i = 0; i < count ; i++ ){
+            resultat += string
+        }
+    }
+    else if (count < 0){
+        resultat = "ERROR"
+    }
+    return resultat
 };
 
 // Do not edit below this line
