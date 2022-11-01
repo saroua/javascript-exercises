9a8199cd79fc0359bedb2e6c9df9c1ae286d5773
Ludovic Orban <luluorban2@hotmail.com>
2022-11-01 23:03:19 +0100

    Second exercice solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = '';
+    let final = string.length;
+    let index = string.length-1;
+    for (let i = 0; i < final; i++){
+        result += string.charAt(index);
+        index--
+    }
+    return result
 };
 
 // Do not edit below this line
