6ad90a2b043219ebadb863172da668638323df95
Ludovic <luluorban2@hotmail.com>
2022-11-03 11:46:47 +0100

    Added solution to ex07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,24 @@
-const ftoc = function() {
+function roundDecimal(number){
+  return Math.round(number *10)/10
+}
 
+const ftoc = function(temp) {
+  let cel = (temp - 32) * (5/9);
+  cel = roundDecimal(cel)
+  console.log(cel)
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let far = (temp * (9/5)) + 32;
+  far = roundDecimal(far);
+  console.log(far);
 };
+
+ctof(37.8)
+ftoc(100)
 
 // Do not edit below this line
-module.exports = {
-  ftoc,
-  ctof
-};
+//module.exports = {
+//  ftoc,
+//  ctof
+//};
