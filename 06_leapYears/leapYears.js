a4b33148c305f8f71804a079de5cb4467015b66b
Ludovic <luluorban2@hotmail.com>
2022-11-03 11:34:15 +0100

    added solution of ex06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,22 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 == 0 && year % 100 == 0){
+        console.log(true);
+        return true;
+    }
+    else if (year % 400 && year % 100 > 0){
+        console.log(false);
+        return(false);
+    }
+    else if(year % 4 == 0 && year % 100 != 0){
+        console.log(true);
+        return true;
+    }
+    else{
+        console.log(false);
+        return false;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = leapYears;
