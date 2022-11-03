2d5c7b6881e47782da3601564674e0740ca23a10
Ludovic Orban <luluorban2@hotmail.com>
2022-11-03 07:09:30 +0100

    added solution for 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(startNumber, endNumber) {
+    let sumFinal = 0;
+    
+    if(startNumber < 0){
+        return "ERROR"
+    }
+    else if (typeof(endNumber) != "number"){
+        return "ERROR"
+    }
+    else if(startNumber > endNumber){
+        let numberToAdd = endNumber;
+        for (let i = endNumber - 1; i < startNumber; i++){
+            sumFinal += numberToAdd;
+            numberToAdd++;
+        }
+    }
+    else{
+        let numberToAdd = startNumber;
+        for (let i = startNumber - 1; i < endNumber; i++){
+            sumFinal += numberToAdd;
+            numberToAdd++;
+        }
+    }
+    return sumFinal;
 };
 
 // Do not edit below this line
